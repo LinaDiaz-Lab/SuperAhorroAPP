d8ab196710fb15789335e87015134c659307e458
LinaDiaz-Lab <linamariadiaz@outlook.es>
2020-12-11 23:02:56 -0500

    Sexto commit

--- src/app/componentes/paginas/categorias/categorias.component.ts
+++ src/app/componentes/paginas/categorias/categorias.component.ts
@@ -1,42 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router'
 import { ProductoService } from '../../../servicios/producto.service'
-import { Producto } from '../../..//interfaces/producto';
-import swal from 'sweetalert';
+import { Producto } from '../../../interfaces/producto'
 
 @Component({
   selector: 'app-categorias',
   templateUrl: './categorias.component.html',
-  styleUrls: ['./categorias.component.scss']
+  styleUrls: ['./categorias.component.scss'],
+  providers: [ProductoService]
 })
 export class CategoriasComponent implements OnInit {
-  productos: Array<Producto> = [];
-
-  constructor(private servicioProductos: ProductoService,
-    ) { 
-    this.cargarProductos()
+  //rutaActiva: any
+  categoria: any;
+  listaCategoria: Producto [] = []
+  productoZoom: Producto = {
+    nombre: '',
+    marca: '', 
+    precio: 0,
+    categoria: '',
+    cantInventario: 0,
+    urlImg: ''
   }
+
+  constructor(
+    private rutaActiva: ActivatedRoute,
+    private _productoService: ProductoService
+  ) { }
 
   ngOnInit(): void {
-    this.cargarProductos()
+    this.rutaActiva.params.subscribe((params: Params) =>{
+      this.categoria = params.categoria;
+      console.log(this.categoria)
+      this._productoService.list().subscribe(
+        response => {
+          let listaAuxiliar: Producto[] = [];
+          for (let i = 0; i < response.length; i++) {
+            if(response[i].categoria === this.categoria){
+              listaAuxiliar.push(response[i])
+            }
+          }
+          this.listaCategoria = listaAuxiliar
+          console.log(this.listaCategoria)
+        },
+        error => {
+          console.log(error)
+        }
+      )
+    })
   }
 
-  cargarProductos(){
-    this.servicioProductos.list()
-    .subscribe(
-      (producto) => {
-        this.productos = producto
-      },
-      (err) => {
-        console.error('Erro al traer los productos ', err);
-            swal(
-              {
-                title: "Error al listar los productos",
-                text: "Intenta de nuevo",
-                icon: "warning",
-                dangerMode: true,
-              });
-      }
-    )
+  onClickZoom(productoSeleccionado: Producto){
+    this.productoZoom = productoSeleccionado
   }
-
 }
